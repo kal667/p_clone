27b3daef7bbc81a2a12d637b1c4750b2f08f473e
kal667 <kkelly.667@gmail.com>
2017-04-24 21:02:08 -0400

    First commit with initial project file and a src directory.

---
+++ project_files/Hello.cpp
@@ -0,0 +1,52 @@
+//===- Hello.cpp - Example code from "Writing an LLVM Pass" ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements two versions of the LLVM "Hello World" pass described
+// in docs/WritingAnLLVMPass.html
+//
+//===----------------------------------------------------------------------===//
+
+#define DEBUG_TYPE "hello"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+
+
+namespace {
+struct Hello :  public FunctionPass
+{
+
+        /** Constructor. */
+	static char ID;                           
+	Hello() : FunctionPass(ID) {}
+
+        //DEFINE_INTPASS_ANALYSIS_ADJUSTMENT(PointerAnalysisPass);
+
+        /**
+         * @brief Runs this pass on the given function.
+         * @param [in,out] func The function to analyze
+         * @return true if the function was modified; false otherwise
+        */
+        virtual bool runOnFunction(llvm::Function &F){
+		errs() << "Hello: " ;
+		errs().write_escaped(F.getName())<< "\n";
+		return false;
+	}
+
+};
+}
+
+char Hello::ID = 0;
+static RegisterPass<Hello> X("hello", "Hello World Pass", false, false);
+
+
+
